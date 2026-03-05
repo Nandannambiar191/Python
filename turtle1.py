d2355dd392ceb721a04c68d7ff79259f4c25bd7b
Nandannambiar191 <nambiarnandan@outlook.com>
2026-03-04 21:09:57 -0600

    Add files via upload

---
+++ turtle1.py
@@ -0,0 +1,10 @@
+import turtle
+turtle.Screen().bgcolor("Blue")
+wh = turtle.Screen()
+wh.setup(1000, 900)
+turtle.title("Nandan's coding")
+po = turtle.Turtle()
+for i in range(4):
+    po.forward(650)
+    po.left(90)
+    i += 1
