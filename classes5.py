121542f4d62cd18daae2144ca010ab77aa4f4e22
Nandannambiar191 <nambiarnandan@outlook.com>
2026-03-24 19:59:53 -0500

    Add files via upload

---
+++ classes5.py
@@ -0,0 +1,13 @@
+"""
+
+Write a program to create a class with the name employee and create a constructor and destructor. 
+Then, create an object for that class and delete the object.
+"""
+class Employee:
+    def __init__(self):
+        print("Employee created")
+    def __del__(self):
+        print("Destructor called, employee deleted")
+
+a = Employee()
+del a
